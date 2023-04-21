71b6460396fc2eb7f5092967c4fea2c33ec35330
samyukt-g <samyukt.g@gmail.com>
2023-04-21 09:41:52 +0530

    Remove all files from the repository

--- footer.php
+++
@@ -1,9 +0,0 @@
-    <footer>
-        <div class="container">
-            <p>&copy; <?php the_date('Y'); ?> - <?php bloginfo('name'); ?></p>        
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
-
-    </body>
-</html>
